25db3919f49df240ef3faed6293747c0ada7f306
Revoa-ux <eezyecom@gmail.com>
2025-07-14 21:47:01 -0500

    Fix ESLint configuration and TypeScript version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:eslint-comments/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-comments'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -16,10 +17,24 @@ module.exports = {
     ],
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-var-requires': 'warn'
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    'no-unused-labels': 'warn',
+    'unused-imports/no-unused-imports': 'off',
+    'unused-imports/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    // Add this rule to solve the "Unused eslint-disable directive" errors
+    'eslint-comments/no-unused-disable': 'off',
   },
   // Add overrides to disable specific rules for specific files or patterns
   overrides: [
+    {
+      files: ['**/*.tsx', '**/*.ts'],
+      rules: {
+        'eslint-comments/no-unused-disable': 'off',
+      },
+    },
     {
       // Disable checking for server-side files that might have type errors during build
       files: ['src/server/**/*.ts', 'src/server/**/*.tsx', 'src/types/**/*.ts'],
