7ed589180a09018ee83cb24162abb02e396818e2
Revoa-ux <eezyecom@gmail.com>
2025-11-27 23:55:36 -0500

    Updated CollapsibleClientProfile.tsx

--- src/pages/admin/Chat.tsx
+++ src/pages/admin/Chat.tsx
@@ -411,7 +411,9 @@ const AdminChat = () => {
           </div>
 
       {/* Chat Area */}
-      <div className="flex-1 flex flex-col">
+      <div className={`flex-1 flex flex-col overflow-hidden ${
+        showUserProfile ? '' : 'rounded-r-xl'
+      }`}>
         {selectedChat ? (
           <>
             {/* Header */}
